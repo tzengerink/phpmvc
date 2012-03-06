1b2a90dcb9c43f0a73103bd0e3b4e51b034cebe8
T. Zengerink <t.zengerink@gmail.com>
2012-03-06 17:33:25 +0100

    moved queries to own directory

--- db/example.create.sql
+++
@@ -1,8 +0,0 @@
-/** -- DROP --------------------------------------------------------------- */
-
-DROP DATABASE IF EXISTS [database_name];
-
-/** -- CREATE ------------------------------------------------------------- */
-
-CREATE DATABASE IF NOT EXISTS [database_name];
-USE [database_name];
---
+++ db/queries/example.create.sql
@@ -0,0 +1,11 @@
+/** -- DROP, CREATE AND USE DB -------------------------------------------- */
+
+DROP DATABASE IF EXISTS `[database_name]`;
+CREATE DATABASE IF NOT EXISTS `[database_name]`;
+USE `[database_name]`;
+
+/** -- CREATE TABLES ------------------------------------------------------ */
+
+/** -- ADD CONSTRAINTS ---------------------------------------------------- */
+
+/** -- INSERTS ------------------------------------------------------------ */
