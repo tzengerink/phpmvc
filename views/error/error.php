71bf441716b334a17446bee2a4405c11b070b36d
T. Zengerink <t.zengerink@gmail.com>
2012-03-06 18:05:02 +0100

    modified error views

--- views/error/error.php
+++ views/error/error.php
@@ -7,9 +7,28 @@
 		<title>Exception - <?php echo $message; ?></title>
 		<style type="text/css">
 			body { color:#333; font-family:"Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Lucida, sans-serif; }
-			#header { background-color:#f50; color:#fff; font-size:18px; padding:8px; }	
+			#header { 
+				background-color:               #dd4b39;
+				-moz-border-radius-topleft:     4px;
+				-moz-border-radius-topright:    4px;
+				-moz-border-radius-bottomright: 0px;
+				-moz-border-radius-bottomleft:  0px;
+				-webkit-border-radius:          4px 4px 0px 0px;
+				border-radius:                  4px 4px 0px 0px;
+				color:                          #fff;
+				font-size:                      18px;
+				padding:                        8px;
+			}	
 			.step { font-size:14px; padding:5px; }
 			.step:nth-child(even) { background-color:#efefef; }
+			.step:last-child { 
+				-moz-border-radius-topleft:     0px;
+				-moz-border-radius-topright:    0px;
+				-moz-border-radius-bottomright: 4px;
+				-moz-border-radius-bottomleft:  4px;
+				-webkit-border-radius:          0px 0px 4px 4px;
+				border-radius:                  0px 0px 4px 4px;
+			}
 			.file { color:#aaa; }
 		</style>
 	</head>
